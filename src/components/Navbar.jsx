02687dbd007794ba2a904c940d17cbb8ef310b5a
Sunny Patel <sunnypatel.koder@gmail.com>
2025-04-11 00:12:07 +0530

    fix: nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -167,18 +167,10 @@ const Navbar = () => {
             <a className="px-5 font-semibold hover:text-[#5c0601] py-7">
               Downloads
             </a>
-            <Link
-              to="/about-us"
-              className="px-5 font-semibold hover:text-[#5c0601] py-7 cursor-pointer"
-            >
-              About Us
-            </Link>
             <Link to="/about-us">
-              <li className="group px-4 py-3 cursor-pointer border-b hover:bg-[#5c0601] transition-all rounded-md">
-                <span className="font-medium text-gray-800 group-hover:text-white transition-all">
-                  About Us
-                </span>
-              </li>
+              <span className="px-5 font-semibold hover:text-[#5c0601] py-7 cursor-pointer">
+                About Us
+              </span>
             </Link>
 
             <a className="px-5 font-semibold hover:text-[#5c0601] py-7 cursor-pointer">
